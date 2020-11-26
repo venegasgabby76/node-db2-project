d1865639ba0936a9eb7aec25847d45c6ca0b0a14
Gabriellia Venegas-Johnson <61035529+venegasgabby76@users.noreply.github.com>
2020-11-26 14:16:01 -0800

    got the project to work but the seeds dont seem to be working correctly

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -4,9 +4,32 @@ const db = require("../data/config")
 
 const router = express.Router()
 
+
 router.get("/cars", async (req, res, next) => {
 	try {
 		res.json(await db("cars"))
+	} catch(err) {
+		next(err)
+	}
+})
+
+router.get("/cars/:id", async (req, res, next) => {
+	try {
+		const { id } = req.params
+		const car = await db("cars").where({ id }).first()
+		
+		res.json(car)
+	} catch(err) {
+		next(err)
+	}
+})
+
+router.post("/cars", async (req, res, next) => {
+	try {
+		const [id] = await db("cars").insert(req.body)
+		const newCar = await db("cars").where({ id }).first()
+
+		res.status(201).json(newCar)
 	} catch(err) {
 		next(err)
 	}
